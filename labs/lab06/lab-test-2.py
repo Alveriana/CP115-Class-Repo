554abc98c7cbd8210f25142de09bd6fecc289a01
Alveriana <alverianamohan@gmail.com>
2025-08-18 13:11:49 +0800

    Python Environment

---
+++ labs/lab06/lab-test-2.py
@@ -0,0 +1,18 @@
+#using appropriate escape characters and arithmetic expressions
+#This code is for lab test 2
+print("Name: Ahmad Bin Abu\t\t\t\tMatric. No: MS2025123499")
+
+
+print("*\t\t\t*")
+print("**\t\t**")
+print("***\t***")
+print("********")
+print("***\t***")
+print("**\t\t**")
+print("*\t\t\t*")
+
+
+print("This is my")
+print("\tsecond")
+print("\t\tassignment")
+print("I want 2x10 marks, which is 20 full marks")
