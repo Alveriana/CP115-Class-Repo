dadd028910e2379f58bcae865a2a41dedc3ef6ba
Alveriana <alverianamohan@gmail.com>
2025-08-18 13:13:07 +0800

    Python Environment and Testing

---
+++ labs/lab07/exercise.py
@@ -0,0 +1,8 @@
+# Old way using string concatenation (from Lab 06)
+student_name = "Ali"
+age = 20
+grade = 85.5
+
+print("Student: " + student_name)
+print("Age: " + str(age))
+print("Grade: " + str(grade) + "%")
