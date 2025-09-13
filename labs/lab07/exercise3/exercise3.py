fb9039ecfe3649d77f4e0ecf5692f4a71e573c86
Alveriana <alverianamohan@gmail.com>
2025-09-13 17:54:29 +0800

    Lab 07

---
+++ labs/lab07/exercise3/exercise3.py
@@ -0,0 +1,14 @@
+import school_data
+
+print(f" Name: {school_data.name}")
+print(f" Student ID: {school_data.student_id}")
+print(f" Age: {school_data.age}")
+print(f" Course Code: {school_data.course_code}")
+print(f" Course Name: {school_data.course_name}")
+print(f" Score 1: {school_data.score_1}")
+print(f" Score 2: {school_data.score_2}")
+print(f" Total Score: {school_data.total_score}")
+print(f" Name Uppercase: {school_data.name_upper}")
+print(f" Name Lowercase: {school_data.name_lower}")
+print(f" Name Length: {school_data.name_length}")
+print(f" Square Root of Total Score: {school_data.square_root:.2f}")
