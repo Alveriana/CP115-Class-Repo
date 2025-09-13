c59c355a731747ebfae73c41ef73bebde2670cb4
Alveriana <alverianamohan@gmail.com>
2025-09-13 17:33:21 +0800

    Lab 08

--- labs/lab08/exercise6/motion_calculator.py
+++ labs/lab08/exercise6/motion_calculator.py
@@ -3,11 +3,22 @@ import physics_calculator
 time = 2.0
 
 # Calculate the time it takes for the ball to hit the ground
-position = physics_calculator.building_height + (physics_calculator.initial_velovity * time) - (0.5 * physics_calculator.gravity * (time ** 2) )
+position = physics_calculator.building_height + (physics_calculator.initial_velocity * time) - (0.5 * physics_calculator.gravity * time ** 2 )
 velocity = physics_calculator.initial_velocity - ( physics_calculator.gravity * time )
-kinetic_energy = 0.5 * physics_calculator.ball_mass * (physics_calculator.velocity ** 2)
+kinetic_energy = 0.5 * physics_calculator.ball_mass * (velocity ** 2)
+
+# Moving status
+if position > 0:
+    moving_status = "Moving up"
+elif position < 0:
+    moving_status = "Moving down"
+else:
+    moving_status = "At rest"
 
 # Print the results
 print(f" Height: {physics_calculator.building_height} m")
-print(f" Velocity: {physics_calculator.velocity} m/s")
+print(f" Velocity: {velocity:.2f} m/s")
 print(f" Mass: {physics_calculator.ball_mass} kg")
+print(f" Position: {position:.2f} m")
+print(f" Energy: {kinetic_energy:.2f} J")
+print(f" Moving Status: {moving_status}")
