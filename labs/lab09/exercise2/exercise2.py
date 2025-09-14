4feb98cfd4e20836112ae96df5c2f4338df4f2d7
Alveriana <alverianamohan@gmail.com>
2025-09-14 13:45:55 +0800

    Lab9

--- labs/lab09/exercise2/exercise2.py
+++ labs/lab09/exercise2/exercise2.py
@@ -1,8 +1,7 @@
 employee_name = input()
-total_income = float(input())
-base_salary = int(input())
+base_salary = float(input())
 overtime_hours = int(input())
-tax_status = input()
+tax_status = input().lower()
 
 overtime_pay = overtime_hours * 35
 total_income = base_salary + overtime_pay
@@ -12,24 +11,23 @@ if tax_status == "single":
     if total_income >= 5000:
         tax_rate = 0.22
     else:
-        tax_rate = 0.12
+        tax_rate = 0.18
 elif tax_status == "married":
     if total_income >= 6000:
         tax_rate = 0.20
     else:
         tax_rate = 0.15
-else:
-    tax_status == "head"
+elif tax_status == "head":
     if total_income >= 5500:
         tax_rate = 0.25
     else:
         tax_rate = 0.19
+else:
+    tax_rate = 0.0  # Default case if tax_status is invalid
 
-tax_amount = total_income * tax_rate
-epf = total_income * 0.11
-socso = total_income * 0.005
-net_salary = total_income - (tax_amount + epf + socso)
+after_tax = total_income * ( 1- tax_rate )
+net_salary = after_tax * (1 - 0.11 - 0.005)
 
 print(employee_name)
-print(tax_rate)
+print(f"{int(tax_rate*100)}%")
 print(f"{net_salary:.2f}")
