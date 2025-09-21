32967cc10cf7d4ccd823d0cb0fbcd2f333a26a20
Alveriana <alverianamohan@gmail.com>
2025-09-21 22:35:48 +0800

    lab9

--- labs/lab09/exercise6/exercise6.py
+++ labs/lab09/exercise6/exercise6.py
@@ -3,6 +3,21 @@ overtime_hours = int(input())
 is_weekend = input()
 
 # TODO: Your code here
+if position == "Manager":
+    base_hourly_rate = 30
+elif position == "Supervisor":
+    base_hourly_rate = 20
+elif position == "Staff":
+    base_hourly_rate = 15
+elif position == "Intern":
+    base_hourly_rate = 8
 
+if overtime_hours <= 8:
+    overtime_pay = overtime_hours * base_hourly_rate * 1.5
+else:
+    overtime_pay = (8 * base_hourly_rate * 1.5) + ((overtime_hours - 8) * base_hourly_rate * 2.0)
+
+if is_weekend == "Yes":
+    overtime_pay += 5 * overtime_hours
 
 print(overtime_pay)
--- labs/lab09/exercise7/exercise7.py
+++ labs/lab09/exercise7/exercise7.py
@@ -1,9 +1,26 @@
 student_gpa = float(input())
 total_score = int(input())
-number_extracurricular = input()
+number_extracurricular = int(input())
 completed_interview = input()
 
 # TODO: Your code here
+requirement_met = 0
+
+if student_gpa >= 3.0:
+    requirement_met += 1
+if total_score >= 1200:
+    requirement_met += 1
+if number_extracurricular >= 1:
+    requirement_met += 1
+if completed_interview == "Yes":
+    requirement_met += 1
+
+if requirement_met == 4:
+    admission_status = "Accepted"
+elif requirement_met == 3:
+    admission_status = "Waitlisted"
+else:
+    admission_status = "Rejected"
 
 
 print(admission_status)
